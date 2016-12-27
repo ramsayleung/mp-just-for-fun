720b25a2e4dfb85690adae9d9c1c9e6445de6236
Samray <samrayleung@gmail.com>
2016-12-27 16:34:42 +0800

    finish token test function,to be continued

---
+++ weixin.py
@@ -0,0 +1,42 @@
+import hashlib
+import xml.etree.ElementTree as ET
+from config import DevConfig
+
+from flask import Flask, make_response, request
+
+app = Flask(__name__)
+app.config.from_object(DevConfig)
+
+
+@app.route('/weixin')
+def weixin():
+    if request.method == 'GET':
+        token = 'samray'
+        data = request.args
+        signature = data.get('signature', '')
+        timestamp = data.get('timestamp', '')
+        nonce = data.get('nonce', '')
+        echostr = data.get('echostr', '')
+        s = [timestamp, nonce, token]
+        s.sort()
+        s = ''.join(s)
+        if (hashlib.sha1(s).hexdigest() == signature):
+            return make_response(echostr)
+        else:
+            rec = request.stream.read()
+            xml_rec = ET.fromstring(rec)
+            tou = xml_rec.find('ToUserName').text
+            fromu = xml_rec.find('FromUserName').text
+            content = xml_rec.find('Content').text
+            xml_rep = "<xml>\
+            <ToUserName><![CDATA[%s]]></ToUserName>\
+            <FromUserName><![CDATA[%s]]></FromUserName>\
+            <CreateTime>%s</CreateTime>\
+            <MsgType><![CDATA[text]]></MsgType>\
+            <Content><![CDATA[%s]]></Content>\
+            <FuncFlag>0</FuncFlag>\
+            </xml>"
+
+
+if __name__ == "__main__":
+    app.run()
